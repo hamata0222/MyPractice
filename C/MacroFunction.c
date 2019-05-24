6c8cd3e206ac593bb4d7aba50c0634faf0cab275
Hiroshi Hamada <hiroshi.hamada.xt@renesas.com>
2019-05-24 16:48:30 +0900

    Added MacroFunction.c

---
+++ C/MacroFunction.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define MACRO_FUNC(name, ret)                       \
+    do {                                            \
+        printf (#name "_phantom is read...");       \
+        ret = (##name##_phantom);                   \
+    } while (0)
+
+int main (void)
+{
+    unsigned int num_phantom = 0xFFFF;
+    unsigned int num = 0x5555;
+    unsigned int val;
+    
+    MACRO_FUNC (num, val);
+    
+    printf ("val = 0x%04X", val);
+}
